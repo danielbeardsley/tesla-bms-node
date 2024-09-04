a13d46609116bc2019f02b9edf58cf571322fb90
Daniel Beardsley <daniel@ifixit.com>
2024-09-04 00:33:13 -0700

    Serial port: fix read semantics after upgrade
    Turns out, sometimes noed.js streams won't do anything when calling
    read() unless you subscribe to 'readable' first.
    
    This means we have to create our own internal buffer of data and then
    read from *that*.

--- src/serial-wrapper.js
+++ src/serial-wrapper.js
@@ -4,20 +4,24 @@ class SerialWrapper {
    constructor(device, speed) {
       this.device = device;
       this.speed = speed;
-      this.readQueue = [];
+      this.buffer = [];
    }
 
    async open() {
       return new Promise((resolve, reject) => {
-         this.port.on('error', function (err) {
-            console.log('Error: ', err.message);
-         });
          this.port = new SerialPort({ path: this.device, baudRate: this.speed }, err => {
             if (err) {
                return reject(err);
             } else {
                return resolve(this);
             }
+         });
+         this.port.on('error', function (err) {
+            console.log('Error: ', err.message);
+         });
+         this.port.on('readable', () => {
+            const data = this.port.read();
+            this.buffer.push(...data)
          });
       });
    }
@@ -41,15 +45,14 @@ class SerialWrapper {
    }
 
    readAll() {
-      let allData = [];
-      while ((data = this.port.read()) !== null) {
-         allData.push(...data);
-      }
-      return allData;
+      const buffer = this.buffer;
+      this.buffer = [];
+      return buffer;
    }
 
+
    flushInput() {
-      this.readQueue = [];
+      this.buffer = [];
    }
 }
 
