e459c9a43a4c9b60ce08c4430db84b187f7c182e
Daniel Beardsley <daniel@ifixit.com>
2025-03-06 23:51:59 -0800

    BMS: Separate work function from work loop
    Also, use timeout *afer* completing each loop so we don't run into
    ourselves if the work process takes longer than the interval.
    
    Change intervalMs to intervalS cause we'll likely alwasy be > 1s.

--- src/bms/bms.ts
+++ src/bms/bms.ts
@@ -17,19 +17,35 @@ class BMS {
         await this.battery.readAll();
     }
 
-    public startMonitoring() {
-        this.timeout = setInterval(() => {
-            this.workLoop();
-        }, this.config.bms.intervalMs);
+    public start() {
+        logger.info(`Starting Battery monitoring every $ds`, this.config.bms.intervalS);
+        if (this.timeout) {
+            throw new Error("BMS already running");
+        }
+        this.workLoop();
     }
 
     public stopMonitoring() {
         clearInterval(this.timeout);
     }
 
-    private workLoop() {
-        this.battery.readAll();
-        const range = this.battery.getCellVoltageRange();
+    private async workLoop() {
+        const now = Date.now();
+        try {
+            logger.debug("Starting work loop");
+            await this.work();
+        } catch (err) {
+            logger.error(err)
+        }
+        logger.debug("Finished work loop in %d ms", Date.now() - now);
+        this.timeout = setTimeout(this.workLoop, this.config.bms.intervalS);
+    }
+
+    private async work() {
+        await this.battery.stopBalancing();
+        await this.battery.readAll();
+        const range = await this.battery.getCellVoltageRange();
+        await this.battery.balance(this.config.bms.intervalS);
         logger.debug(`Cell voltage spread:${(range.spread*1000).toFixed(0)}mV range: ${range.min.toFixed(3)}V - ${range.max.toFixed(3)}V`);
     }
 }
