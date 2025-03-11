b36dde9c2b766c992ce32d19155ea4abc0715cf8
Daniel Beardsley <daniel@ifixit.com>
2025-03-10 22:14:39 -0700

    Pylontech: Add tests for encoding frames

--- src/inverter/pylontech-frame.test.ts
+++ src/inverter/pylontech-frame.test.ts
@@ -23,3 +23,20 @@ describe('decodeFrame', () => {
         expect(() => decodeFrame(Buffer.from("~XX0000~"))).toThrow();
     });
 });
+
+describe('encodeFrame', () => {
+   it('should encode an empty frame', () => {
+      const result = encodeFrame(Buffer.alloc(0));
+      expect(result).toEqual(Buffer.from("~0000~"));
+   });
+
+   it('should encode a frame with data', () => {
+      const result = encodeFrame(Buffer.from("123ABC"));
+      expect(result).toEqual(Buffer.from("~123ABCFEA4~"));
+   });
+
+   it('should handle long frames', () => {
+      const result = encodeFrame(Buffer.from("asdlfkjawefwiafjoi4jfqoi34fjwlefjaliejfaw"));
+      expect(result).toEqual(Buffer.from("~asdlfkjawefwiafjoi4jfqoi34fjwlefjaliejfawEFB6~"));
+   });
+});
