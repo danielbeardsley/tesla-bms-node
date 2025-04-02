dd4a747cb496e00234ba02a551d85208afd419f8
Daniel Beardsley <daniel@ifixit.com>
2025-04-01 18:37:53 -0700

    Charging: Smooth out the bumps
    We can easily have oscillations in the charge current as a high current
    results in a high voltage, which then results in lowering the charge
    current. This can result in wild ups and downs from 100A to 0 over the
    course of 10s or so.
    
    Let's smooth it out using a simple formula: new = old * 0.9 + current * 0.1
    for charge current and cell voltage range.
    
    Note: That this formula gets applied every time the inverter asks about
    the charge info (about once per 5-10s).

--- src/bms/charging/voltage-a.ts
+++ src/bms/charging/voltage-a.ts
@@ -6,8 +6,11 @@ import { clamp } from "../..//utils";
 import { ChargingModule } from "./charging-module";
 
 export class VoltageA implements ChargingModule {
-    private battery: BatteryI;
-    private config: Config;
+   private battery: BatteryI;
+   private config: Config;
+   private chargeCurrentSmoothed: number|null = null;
+   private cellVoltMaxSmoothed: number|null = null;
+   private cellVoltMinSmoothed: number|null = null;
 
    constructor(config: Config, battery: BatteryI) {
       this.battery = battery;
@@ -20,6 +23,9 @@ export class VoltageA implements ChargingModule {
 
    getChargeDischargeInfo(): ChargeInfo {
       const cellVoltageRange = this.battery.getCellVoltageRange();
+      this.cellVoltMinSmoothed = this.smooth(this.cellVoltMinSmoothed, cellVoltageRange.min);
+      this.cellVoltMaxSmoothed = this.smooth(this.cellVoltMaxSmoothed, cellVoltageRange.max);
+
       inverterLogger.debug("Voltage range: %d - %d", cellVoltageRange.min, cellVoltageRange.max);
       // Scale down the charging current as the highest volt cell
       // gets within "buffer" volts of the maxCellVolt setting
@@ -28,13 +34,25 @@ export class VoltageA implements ChargingModule {
       const bufferStart = maxCellVolt - buffer;
       const chargeScale = 1 - clamp((cellVoltageRange.max - bufferStart) / buffer, 0, 1);
 
+      // Scale down the charging current as the highest volt cell
+      // gets within "buffer" volts of the maxCellVolt setting
+      const minCellVolt = this.config.battery.discharging.minCellVolt;
+      const bufferMinStart = minCellVolt + buffer;
+      const dischargeScale = 1 - clamp((bufferMinStart - cellVoltageRange.min) / buffer, 0, 1);
+
+      this.chargeCurrentSmoothed = this.smooth(this.chargeCurrentSmoothed, this.config.battery.charging.maxAmps * chargeScale);
+
       return {
          chargeVoltLimit: this.config.battery.charging.maxVolts,
          dischargeVoltLimit: this.config.battery.discharging.minVolts,
-         chargeCurrentLimit: this.config.battery.charging.maxAmps * chargeScale,
-         dischargeCurrentLimit: this.config.battery.discharging.maxAmps,
-         chargingEnabled: cellVoltageRange.max < this.config.battery.charging.maxCellVolt,
-         dischargingEnabled: cellVoltageRange.min > this.config.battery.discharging.minCellVolt,
+         chargeCurrentLimit: this.chargeCurrentSmoothed,
+         dischargeCurrentLimit: this.config.battery.discharging.maxAmps * dischargeScale,
+         chargingEnabled: this.cellVoltMaxSmoothed < this.config.battery.charging.maxCellVolt,
+         dischargingEnabled: this.cellVoltMinSmoothed > this.config.battery.discharging.minCellVolt,
       };
+   }
+
+   private smooth(prev: number|null, newVal: number): number {
+      return prev === null ? newVal : prev * 0.9 + newVal * 0.1;
    }
 }
