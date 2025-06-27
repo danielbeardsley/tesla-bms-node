f20b804c2daa152ed9b3cbf6ea7b5dc47b3f07c6
Daniel Beardsley <daniel@ifixit.com>
2025-06-27 02:18:59 -0700

    Canbus: add all message types
    Well, at least all the ones in the spec.
    
    Leave alarms packet as all 0s until we're ready to implement it for
    reals.

--- src/inverter/can-packet.ts
+++ src/inverter/can-packet.ts
@@ -1,7 +1,11 @@
 import { SmartBuffer } from 'smart-buffer';
 import { inverterLogger as logger } from "../logger";
+import { createRawChannel } from 'socketcan';
+import type { BatteryI } from '../battery/battery';
 
-export enum CanMsgType {
+type RawChannel = ReturnType<typeof createRawChannel>;
+
+enum CanMsgType {
    Alarms = 0x359,
    ChargeParams = 0x351,
    SOC = 0x355,
@@ -10,7 +14,7 @@ export enum CanMsgType {
    InverterReply = 0x305,
 };
 
-export type AlarmsParams = {
+type AlarmsParams = {
    // Protection
    dischargeOverCurrent: boolean,
    chargeOverCurrent: boolean,
@@ -30,24 +34,76 @@ export type AlarmsParams = {
    internalCommunicationFail: boolean,
 }
 
-export function chargeParamsPacket(data: ChargeInfo) {
-   const out = new SmartBuffer();
-   out.writeUInt16LE(volts(data.chargeVoltLimit));
-   out.writeInt16LE(amps(data.chargingEnabled ? data.chargeCurrentLimit : 0));
-   out.writeInt16EE(amps(data.dischargingEnabled ? data.dischargeCurrentLimit : 0));
-   out.writeUInt16LE(volts(data.dischargeVoltLimit));
-   return [CanMsgType.ChargeParams, out];
+export function sendAllPackets(channel: RawChannel, chargeData: ChargeInfo, battery: BatteryI) {
+   channel.send(message(chargeParamsPacket(chargeDatt)));
+   channel.send(message(stateOfChargePacket(battery)));
+   channel.send(message(packVoltagePacket(battery)));
+   channel.send(message(alarmsPacket(battery)));
 }
 
-export function alarmsPacket() {
-   const out = new SmartBuffer();
+function chargeParamsPacket(data: ChargeInfo) {
+   const out = buf();
+   out.writeUInt16LE(volts(data.chargeVoltLimit));
+   out.writeUInt16LE(amps(data.chargingEnabled ? data.chargeCurrentLimit : 0));
+   out.writeUInt16LE(amps(data.dischargingEnabled ? data.dischargeCurrentLimit : 0));
+   out.writeUInt16LE(volts(data.dischargeVoltLimit));
+   return {
+      id: CanMsgType.ChargeParams,
+      data: out.toBuffer(),
+   };
+}
+
+function stateOfChargePacket(battery: BatteryI) {
+   const out = buf();
+   out.writeUInt16LE(percent(battery.getStateOfCharge()));
+   out.writeUInt16LE(percent(battery.getStateOfHealth()));
+   const vRange = battery.getCellVoltageRange();
+   out.writeUInt16LE(millivolts(vRange.max));
+   out.writeUInt16LE(millivolts(vRange.min));
+   return {
+      id: CanMsgType.SOC,
+      data: out.toBuffer(),
+   };
+}
+
+function packVoltagePacket(battery: BatteryI) {
+   const out = buf();
+   out.writeUInt16LE(Math.round(battery.getVoltage() * 100));
+   out.writeUInt16LE(amps(battery.getCurrent()));
+   const tempRange = battery.getCellVoltageRange();
+   out.writeUInt16LE(temp(tempRange.max));
+   out.writeUInt16LE(temp(tempRange.min));
+   return {
+      id: CanMsgType.VoltsAndTemps,
+      data: out.toBuffer(),
+   };
+}
+
+function alarmsPacket(_battery: BBatteyI) {
+   const out = buf();
    return [CanMsgType.Alarms, out];
 }
 
 function volts(v: number) {
-   return 10 * v;
+   return Math.round(10 * v);
+}
+
+function millivolts(v: number) {
+   return Math.round(1000 * v);
 }
 
 function amps(v: number) {
-   return 10 * v;
+   return Math.round(10 * v);
+}
+
+function percent(portion: number) {
+   return Math.round(100 * portion);
+}
+
+function temp(t: number) {
+   return Math.round(t * 10);
+}
+
+function buf() {
+   return SmartBuffer.fromSize(8);
 }
