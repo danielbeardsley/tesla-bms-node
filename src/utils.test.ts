398e73cceda1b9ac333086e24e366af8e65b4c4b
Daniel Beardsley <daniel@ifixit.com>
2025-10-01 09:42:06 -0700

    Utils: add tests for stickyBool

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { ramp, clamp } from './utils';
+import { ramp, clamp, stickyBool, sleep } from './utils';
 
 describe("utils", () => {
    describe("ramp()", () => {
@@ -26,6 +26,35 @@ describe("utils", () => {
          expect(clamp(19, 10, 20)).toBe(19);
          expect(clamp(3253, 10, 20)).toBe(20);
          expect(clamp(-3434, 10, 20)).toBe(10);
+      });
+   });
+
+   describe("stickyBool()", () => {
+      it("should start int the initial state", () => {
+         let sb = stickyBool(true, 1, 1);
+         expect(sb.get()).toBe(true);
+         sb = stickyBool(false, 1, 1);
+         expect(sb.get()).toBe(false);
+      });
+
+      it("should stick to true as long as configured", async () => {
+         const sb = stickyBool(false, 0.005, 0);
+         sb.set(true);
+         expect(sb.get()).toBe(true);
+         sb.set(false);
+         expect(sb.get()).toBe(true);
+         await sleep(5);
+         expect(sb.get()).toBe(false);
+      });
+
+      it("should stick to false as long as configured", async () => {
+         const sb = stickyBool(true, 0, 0.005);
+         sb.set(false);
+         expect(sb.get()).toBe(false);
+         sb.set(true);
+         expect(sb.get()).toBe(false);
+         await sleep(5);
+         expect(sb.get()).toBe(true);
       });
    });
 });
